9e63a00ca1c4d3a3bd077bad3b0a1589ec90ee65
Brahms <brahmsisme@gmail.com>
2024-04-25 00:56:45 +0200

    Test Constructor without price of the class Show

--- src/test/java/dev/brahms/ShowTest.java
+++ src/test/java/dev/brahms/ShowTest.java
@@ -13,11 +13,19 @@ class ShowTest {
 
         Show show = new Show("Concert Rasta", "2023-09-04T20:00", 50d);
 
-        assertTrue(show instanceof Searchable);
         assertTrue(show instanceof Evenement);
         assertEquals("Concert Rasta", show.getTexte());
         assertEquals("2023-09-04T20:00", show.getDateEvent().toString());
         assertEquals(50d, show.getPrice());
+
+    }
+
+    @Test
+    void testShowConstructorWithoutPrice() {
+
+        Show show = new Show("Fête de la musique", "2023-06-21T18:30");
+
+        assertEquals(0d, show.getPrice());
 
     }
 
