a675ad1f0f2157ab420d092079e361f31f0d1940
Brahms <brahmsisme@gmail.com>
2024-04-25 00:54:03 +0200

    Test Constructor of the class Show

--- src/test/java/dev/brahms/ShowTest.java
+++ src/test/java/dev/brahms/ShowTest.java
@@ -1,7 +1,25 @@
 package dev.brahms;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ShowTest {
+
+    @Test
+    void testShowConstructor() {
+
+        Show show = new Show("Concert Rasta", "2023-09-04T20:00", 50d);
+
+        assertTrue(show instanceof Searchable);
+        assertTrue(show instanceof Evenement);
+        assertEquals("Concert Rasta", show.getTexte());
+        assertEquals("2023-09-04T20:00", show.getDateEvent().toString());
+        assertEquals(50d, show.getPrice());
+
+    }
+
 
 }
