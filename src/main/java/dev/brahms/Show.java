06ee7d2ba80db1648c06f8e2d2878feb5af25188
Brahms <brahmsisme@gmail.com>
2024-04-25 00:40:00 +0200

    JavaDoc for the class Show

--- src/main/java/dev/brahms/Show.java
+++ src/main/java/dev/brahms/Show.java
@@ -10,17 +10,41 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.TreeMap;
 
+/**
+ *  Represents a SHOW, which is a type of
+ *  paid or free event involving several artists.
+ */
 public class Show  extends Evenement implements Searchable{
 
+    /**
+     * This is the price about the Show
+     */
     private double price;
+
+    /**
+     *  List of artists
+     */
     private ArrayList<Artist> artists;
 
+    /**
+     * Create a Show
+     * @param texte title of the Show
+     * @param dateEvent date and time of the Show
+     * @param price price of the Show
+     * @param artists list of artists invited to the Show
+     */
     public Show(String texte, LocalDateTime dateEvent, double price, ArrayList<Artist> artists) {
         super(texte, dateEvent);
         this.price = price;
         this.artists = artists;
     }
 
+    /**
+     * Create a Show without artists by default
+     * @param texte title of the Show
+     * @param dateEvent date and time of the Show
+     * @param price price of the Show
+     */
     public Show(String texte, String dateEvent, double price) {
         super(texte, LocalDateTime.parse(dateEvent));
 
@@ -32,26 +56,51 @@ public class Show  extends Evenement implements Searchable{
         this.artists = new ArrayList<>();
     }
 
+    /**
+     * Create a Show free by default
+     * @param texte title of the Show
+     * @param dateEvent date and time of the Show
+     */
     public Show(String texte, String dateEvent) {
         this(texte, dateEvent, 0d);
     }
 
+    /**
+     * Returns the price of the Show
+     * @return price of the Show
+     */
     public double getPrice() {
         return price;
     }
 
+    /**
+     * Updates the price of the Show
+     * @param price new price of the Show
+     */
     public void setPrice(double price) {
         this.price = price;
     }
 
+    /**
+     * Returns the list of artists of the Show
+     * @return list of artists of the Show
+     */
     public ArrayList<Artist> getArtists() {
         return artists;
     }
 
+    /**
+     * Update the list of artists of the Show
+     * @param artists new list artists of the Show
+     */
     public void setArtists(ArrayList<Artist> artists) {
         this.artists = artists;
     }
 
+    /**
+     * Returns a literal representation of the Show
+     * @return representation of the Show
+     */
     @Override
     public String toString() {
 
@@ -93,6 +142,12 @@ public class Show  extends Evenement implements Searchable{
                 + strBuild.toString();
     }
 
+    /**
+     * Returns the list of artists of a given style
+     *
+     * @param s the musical style of sought-after artists
+     * @return the list of artists of a given style
+     */
     @Override
     public List findBy(String s) {
 
