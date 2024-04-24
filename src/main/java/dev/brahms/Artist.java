8d00c9483444840a802bf13fb880074c9ead2679
Brahms <brahmsisme@gmail.com>
2024-04-24 23:55:33 +0200

    creation of attributes, constructor, getters & setters, and toString for the Artist class

--- src/main/java/dev/brahms/Artist.java
+++ src/main/java/dev/brahms/Artist.java
@@ -1,4 +1,60 @@
 package dev.brahms;
 
 public class Artist {
+
+    private String pseudo;
+    private String firstname;
+    private String lastname;
+    private Style style;
+
+    public Artist(String pseudo, String firstname, String lastname, Style style) {
+        super();
+        this.pseudo = pseudo;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.style = style;
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Style getStyle() {
+        return style;
+    }
+
+    public void setStyle(Style style) {
+        this.style = style;
+    }
+
+    @Override
+    public String toString() {
+        return "Artist{" +
+                "pseudo='" + pseudo + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", style=" + style +
+                '}';
+    }
+
 }
