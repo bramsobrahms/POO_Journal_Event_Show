4f270755688c9988923be1cb31fe8aa90b969563
Brahms <brahmsisme@gmail.com>
2024-04-24 19:19:14 +0200

    creation of attributes, constructor and toString for the Journal class

--- src/main/java/dev/brahms/Journal.java
+++ src/main/java/dev/brahms/Journal.java
@@ -1,4 +1,27 @@
 package dev.brahms;
 
+import java.util.ArrayList;
+
 public class Journal {
+
+    private ArrayList<Evenement> events;
+
+    public Journal(ArrayList<Evenement> events) {
+        this.events = events;
+    }
+
+    public ArrayList<Evenement> getEvents() {
+        return events;
+    }
+
+    public void setEvents(ArrayList<Evenement> events) {
+        this.events = events;
+    }
+
+    @Override
+    public String toString() {
+        return "Journal{" +
+                "events=" + events +
+                '}';
+    }
 }
